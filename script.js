b9f94dc21cae231fb5941dbf5dca106038809bf9
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-09-26 23:01:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+const openBtn = document.getElementById('openModal');
+    const modal = document.getElementById('myModal');
+    const closeBtn = document.getElementById('closeBtn');
+    
+    openBtn.onclick = () => {
+      modal.classList.add('open');
+    };
+    closeBtn.onclick = () => {
+      modal.classList.remove('open');
+    };
+    window.onclick = (e) => {
+      if (e.target === modal) {
+        modal.classList.remove('open');
+      }
+    };
